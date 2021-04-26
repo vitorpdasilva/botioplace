b919f5767a330f1d7b3b2a9b222d956fe5a3216c
vitorboccio <vitorboccio@gmail.com>
2021-04-26 15:21:12 -0400

    init amplify

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Amplify from 'aws-amplify';
+import aws_exports from './aws-exports';
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
 // Bring in default Element React theme
 import "element-theme-default";
+
+Amplify.configure(aws_exports);
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
