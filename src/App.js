e95de31f9bf8fe38a995ce8dd7b2ebd5e70d765b
vitorboccio <vitorboccio@gmail.com>
2021-04-27 12:04:05 -0400

    updating dependencies

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Amplify, { Auth, Hub } from 'aws-amplify';
+import { Authenticator, AmplifyTheme } from 'aws-amplify-react';
+
+import aws_exports from './aws-exports';
 import "./App.css";
 
-class App extends React.Component {
-  state = {};
 
-  render() {
-    return <div>App</div>;
-  }
+Amplify.configure(aws_exports);
+
+const App = () => {
+  const [user, setUser] = useState(null);
+  console.log({ hub: Hub });
+  useEffect(() => {
+    const getUserData = async () => {
+      const user = await Auth.currentAuthenticatedUser();
+      setUser(user || null);
+    }
+    getUserData();
+  }, []);
+
+  if (!user) return <Authenticator theme={theme} />
+  return <div>App</div>;
+
 }
 
+const theme = {
+  ...AmplifyTheme,
+}
+
+// export default WithAuthenticator(App, true, [], null, theme);
 export default App;
